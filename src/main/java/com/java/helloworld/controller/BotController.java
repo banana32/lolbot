fd6c0c7c890b4ce7be6f92bff17c7d991a430f53
sangmoon <cholsoo22001@gmail.com>
2018-05-04 16:24:48 +0900

    keyboardVO 추가. 서버 배포 완료. 카톡봇 작동 시작

--- src/main/java/com/java/helloworld/controller/BotController.java
+++ src/main/java/com/java/helloworld/controller/BotController.java
@@ -1,16 +1,13 @@
 package com.java.helloworld.controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-public class MessageController {
+public class BotController {
     @PostMapping(value = "message", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseMessageVO keyBoardApi(@RequestBody RequestMessageVO req_vo) {
+    public ResponseMessageVO message(@RequestBody RequestMessageVO req_vo) {
         System.out.println(req_vo);
 
         ResponseMessageVO res_vo = new ResponseMessageVO();
@@ -19,5 +16,12 @@ public class MessageController {
         mes_vo.setText("hello " + req_vo.getContent());
         res_vo.setMessage(mes_vo);
         return res_vo;
+    }
+
+    @GetMapping(value="keyboard")
+    public KeyboardVO keyboard(){
+        KeyboardVO keyboard = new KeyboardVO(new String[]{"어제 전적", "최근 전적", "안녕 안녕"});
+
+        return keyboard;
     }
 }
