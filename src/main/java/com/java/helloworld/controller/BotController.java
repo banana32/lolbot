8da05b82e5a1cb7c50962e370f22d623e547d71c
sangmoon <cholsoo22001@gmail.com>
2018-06-01 19:50:38 +0900

    메세지 이름 수정

--- src/main/java/com/java/helloworld/controller/BotController.java
+++ src/main/java/com/java/helloworld/controller/BotController.java
@@ -13,14 +13,24 @@ public class BotController {
         ResponseMessageVO res_vo = new ResponseMessageVO();
         MessageVO mes_vo = new MessageVO();
 
-        mes_vo.setText("hello idiot " + req_vo.getContent());
+        switch(req_vo.getContent()) {
+            case "어제 전적":
+                mes_vo.setText("이것은 어제 전적입니다.");
+                break;
+            case "최근 전적":
+                mes_vo.setText("이것은 최근 전적입니다.");
+                break;
+            default:
+                mes_vo.setText("아직 미구현 기능 입니다.");
+                break;
+        }
         res_vo.setMessage(mes_vo);
         return res_vo;
     }
 
     @GetMapping(value="keyboard")
     public KeyboardVO keyboard(){
-        KeyboardVO keyboard = new KeyboardVO(new String[]{"어제 전적", "최근 전적", "안녕 안녕"});
+        KeyboardVO keyboard = new KeyboardVO(new String[]{"어제 전적", "최근 전적"});
 
         return keyboard;
     }
